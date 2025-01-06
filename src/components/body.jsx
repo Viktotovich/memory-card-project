cca833e855278b61981c015d2c5a4d36e15bed2b
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-06 13:44:05 +0400

    Trouble with fetching data - testing solutions. Current iteration is reccomended by SO and React useEffect docs, with useState being a way to "save" values through useEffect

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react";
+import pokemonArray from "./pokemon-array";
+
 export default function Body() {
+  /*OOOOOOOHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH, WE CAN ADD LOADING ICONSSSSSSSSS */
+  const [pokemons, setPokemons] = useState([]);
+
   return (
     <section>
       <div>Stuff here</div>
@@ -6,4 +12,41 @@ export default function Body() {
   );
 }
 
-//useEffect and useMemo
+function PokeCard() {
+  return (
+    <div className="card-container">
+      <div className="pokemon-container">
+        <div className="p-header">Pokemon Name</div>
+        <div className="p-health">HP 100</div>
+        <div className="p-image">
+          <img src="/fake-a-mon.webp" alt="fake pokemon" />
+        </div>
+        <div className="p-ability">Cuddle</div>
+      </div>
+    </div>
+  );
+}
+
+/* 
+
+export default function Body() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async () => {
+      const pokemonData = [];
+      for (const pokemon of pokemonArray) {
+        const pJSON = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        await pokemonData.push(pJSON)
+      }
+    };
+  }, []);
+
+
+  return (
+    <section>
+      <div>Stuff here</div>
+    </section>
+  );
+}
+*/
---
+++ src/components/pokemon-array.js
@@ -0,0 +1,15 @@
+const pokemonArr = [
+  "ditto",
+  "pikachu",
+  "torchic",
+  "combusken",
+  "blaziken",
+  "zigzagoon",
+  "linoone",
+  "poochyena",
+  "mightyena",
+  "wailord",
+  "horsea",
+];
+
+export default { pokemonArr };
